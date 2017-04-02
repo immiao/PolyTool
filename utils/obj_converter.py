1410e1de0c2764400043cca4e50d9059a53f8cd3
immiao <miaokaixiang@gmail.com>
2017-04-01 20:32:06 -0400

    visualized vertices indices

--- utils/obj_converter.py
+++ utils/obj_converter.py
@@ -107,5 +107,5 @@ def writeToFile(filename):
 		file.write("\n")
 		
 if __name__ == "__main__":
-	convert("../model/obj_test3.obj")
-	writeToFile("../model/obj_test3_converted.obj")
+	convert("../model/obj_test5.obj")
+	writeToFile("../model/obj_test5_converted.obj")
